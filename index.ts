3aa7f84baca9dc4716be76808d23c6e95c343c49
Neozaru <neozaru@mailoo.org>
2020-06-26 11:08:53 +1200

    Rebalance policies

--- index.ts
+++ index.ts
@@ -34,51 +34,100 @@ async function validatorsInfo(near, epochId) {
 }
 
 const seatBalanceConfig = {
-  // targetRatio: 1.5,
-  // thresholdRatios: {
-    checkInterval: 600,
+    rebalancePolicy: {
+      type: 'BEST',
+      minRebalanceAmount: '1000000000000000000000000000'
+    },
+    checkInterval: 300,
     lowThreshold: 1.1,
     lowTarget: 1.2,
     highTarget: 1.8,
     highThreshold: 1.9,
-  // },
 }
 
 function c2h(value) {
-  return value / Math.pow(10,24)
+  return value.toString() / Math.pow(10,24)
 } 
 
-function generateAction(seatBalanceConfig, seatPrice, stakedInPool, stakedByWarchest) {
+function generateProposedAction(seatBalanceConfig, seatPrice, totalStakedInPool) {
   // console.log('args', arguments)
-  const stakeSeatPriceRatio = stakedInPool / seatPrice;
+  const stakeSeatPriceRatio = totalStakedInPool / seatPrice;
   // const targetPoolStake = seatBalanceConfig.targetRatio * seatPrice;
   // console.log('stakeSeatPriceRatio', stakeSeatPriceRatio)
   // const psycho = new BN((seatBalanceConfig.thresholdRatios.low*100))
   // console.log('seatBalanceConfig.thresholdRatios.low', seatBalanceConfig.thresholdRatios.low,seatBalanceConfig.thresholdRatios.low * seatPrice)
   if (stakeSeatPriceRatio < seatBalanceConfig.lowThreshold) {
     const targetAmount = seatPrice.muln(seatBalanceConfig.lowTarget);
-    const diffAmount = targetAmount.isub(stakedInPool).toString();
+    const diffAmount = targetAmount.sub(totalStakedInPool);
     return {
       method: 'stake',
       amount: diffAmount,
-      debug: seatBalanceConfig.lowTarget * seatPrice - stakedInPool,
-      meta: `Pool stake ratio ${stakeSeatPriceRatio} (${c2h(stakedInPool)}) below threshold of ${seatBalanceConfig.lowThreshold}. Staking ${c2h(diffAmount)} more tokens to get to target of ${seatBalanceConfig.lowTarget} (${c2h(targetAmount)})`
+      debug: seatBalanceConfig.lowTarget * seatPrice - totalStakedInPool,
+      meta: `Pool stake ratio ${stakeSeatPriceRatio} (${c2h(totalStakedInPool)}) below threshold of ${seatBalanceConfig.lowThreshold}. Staking ${c2h(diffAmount)} more tokens to get to target of ${seatBalanceConfig.lowTarget} (${c2h(targetAmount)})`
     }
     // console.log('PROPOSAL', res)
     // return res;
   } else if (stakeSeatPriceRatio > seatBalanceConfig.highThreshold) {
     const targetAmount = seatPrice.muln(seatBalanceConfig.highTarget);
-    const diffAmount = stakedInPool.sub(targetAmount).toString();
+    const diffAmount = totalStakedInPool.sub(targetAmount);
     return {
       method: 'unstake',
       amount: diffAmount,
-      debug: stakedInPool - seatBalanceConfig.highTarget * seatPrice,
-      meta: `Pool stake ratio ${stakeSeatPriceRatio} (${c2h(stakedInPool)}) above threshold of ${seatBalanceConfig.highThreshold}. Untaking ${c2h(diffAmount)} more tokens to get to target of ${seatBalanceConfig.highTarget} (${c2h(targetAmount)})`
+      debug: totalStakedInPool - seatBalanceConfig.highTarget * seatPrice,
+      meta: `Pool stake ratio ${stakeSeatPriceRatio} (${c2h(totalStakedInPool)}) above threshold of ${seatBalanceConfig.highThreshold}. Untaking ${c2h(diffAmount)} more tokens to get to target of ${seatBalanceConfig.highTarget} (${c2h(targetAmount)})`
 
     }
     // console.log('PROPOSAL', res)
     // return res;
   }
+}
+
+// function generateActionToExecuteFOK(proposedAction, warchestAccountStakedBalance, warchestAccountUnstakedBalance) {
+//   if (proposedAction.method === 'unstake' && warchestAccountStakedBalance < proposedAction.amount) {
+//     console.warn(`Rebalancing policy set to ${seatBalanceConfig.rebalancePolicy.type} and warchest staked balance (${c2h(warchestAccountStakedBalance)}) < proposed unstake amount (${c2h(proposedAction.amount)})`)
+//     return false;
+//   }
+//   if (proposedAction.method === 'stake' && warchestAccountUnstakedBalance < proposedAction.amount) {
+//     console.warn(`Rebalancing policy set to ${seatBalanceConfig.rebalancePolicy.type} and warchest unstaked balance (${c2h(warchestAccountUnstakedBalance)}) < proposed stake amount (${c2h(proposedAction.amount)})`)
+//     return false;
+//   }
+//   return proposedAction;
+
+// }
+
+function generateActionToExecute(seatBalanceConfig, proposedAction, warchestAccountStakedBalance, warchestAccountUnstakedBalance) {
+  if (proposedAction.method === 'unstake' && warchestAccountStakedBalance.lt(proposedAction.amount)) {
+    console.warn(`Rebalancing policy set to ${seatBalanceConfig.rebalancePolicy.type} and warchest staked balance (${c2h(warchestAccountStakedBalance)}) < proposed unstake amount (${c2h(proposedAction.amount)})`)
+    if (seatBalanceConfig.rebalancePolicy.type === 'BEST') {
+      if (warchestAccountStakedBalance >= seatBalanceConfig.rebalancePolicy.minRebalanceAmount) {
+        // console.log(`Executing best possible action ${proposedAction.method} with amount ${c2h(warchestAccountStakedBalance)}`) 
+        return {
+          method: proposedAction.method,
+          amount: warchestAccountStakedBalance,
+        };
+      }
+    }
+    return false;
+  }
+  if (proposedAction.method === 'stake' && warchestAccountUnstakedBalance.lt(proposedAction.amount)) {
+    console.warn(`Rebalancing policy set to ${seatBalanceConfig.rebalancePolicy.type} and warchest unstaked balance (${c2h(warchestAccountUnstakedBalance)}) < proposed stake amount (${c2h(proposedAction.amount)})`)
+    if (seatBalanceConfig.rebalancePolicy.type === 'BEST') {
+      if (warchestAccountUnstakedBalance >= seatBalanceConfig.rebalancePolicy.minRebalanceAmount) {
+        // console.log(`Executing best possible action ${proposedAction.method} with amount ${c2h(warchestAccountUnstakedBalance)}`) 
+        return {
+          method: proposedAction.method,
+          amount: warchestAccountUnstakedBalance,
+        };
+      }
+    }
+    return false;
+  }
+  return proposedAction;
+  // if (seatBalanceConfig.rebalancePolicy.type === 'FOK') {
+  //   return generateActionToExecuteFOK(proposedAction, warchestAccountStakedBalance, warchestAccountUnstakedBalance);
+  // } else if (seatBalanceConfig.rebalancePolicy.type === 'BEST') {
+
+  // }
 }
 
 function reqSeatPrice(near) {
@@ -92,22 +141,21 @@ function reqSeatPrice(near) {
   // console.log('validators', validatorsRes)
 }
 
-// function reqPoolStake(near) {
-//   return near.connection.provider.query(`account/neozaru.stakehouse.betanet`, "").then((poolState) => {
-//     return new BN(poolState.locked);
-//   })
-// }
 
-function reqPoolStake(account, poolId) {
+function reqPoolGetTotalStakedBalance(account, poolId) {
   return account.viewFunction(poolId, 'get_total_staked_balance', null).then((res) => new BN(res))
 }
 
-function reqAccountStakedBalance(account, accountId, poolId) {
+function reqPoolGetAccountStakedBalance(account, accountId, poolId) {
   return account.viewFunction(poolId, 'get_account_staked_balance', {account_id: accountId}).then((res) => new BN(res))
 }
 
+function reqPoolGetAccountUnstakedBalance(account, accountId, poolId) {
+  return account.viewFunction(poolId, 'get_account_unstaked_balance', {account_id: accountId}).then((res) => new BN(res))
+}
+
 function executeAction(account, action, contractId) {
-  return account.functionCall(contractId, action.method, {amount: action.amount})
+  return account.functionCall(contractId, action.method, {amount: action.amount.toString()})
 }
 
 const ACCOUNT_ID = 'neozaru14.betanet';
@@ -115,33 +163,48 @@ const POOL_ID = 'neozaru.stakehouse.betanet';
 
 async function rebalance() {
   console.log(moment().format() + ' - Rebalancing');
+  // console.log(seatBalanceConfig)
 
   const near = await nearApi.connect(config);
   const account = await near.account(ACCOUNT_ID)
 
   Promise.all([
     reqSeatPrice(near),
-    reqPoolStake(account, POOL_ID),
-    reqAccountStakedBalance(account, ACCOUNT_ID, POOL_ID)
-  ]).then(([seatPrice, poolStake, accountPoolStake]) => {
-    console.log('seatPrice', seatPrice.toString(), 'poolStake', poolStake.toString(), 'accountPoolStake', accountPoolStake.toString())
-    const proposedAction = generateAction(seatBalanceConfig, seatPrice, poolStake, accountPoolStake)
+    reqPoolGetTotalStakedBalance(account, POOL_ID),
+    
+  ]).then(([seatPrice, poolStake]) => {
+    console.log('seatPrice', c2h(seatPrice), 'poolStake', c2h(poolStake), 'accountPoolStake', 'ratio', poolStake / seatPrice)
+    const proposedAction = generateProposedAction(seatBalanceConfig, seatPrice, poolStake)
     if (!proposedAction) {
       console.log('Not action proposed. Everything seems to be all right.')
       return;
     }
     console.log('Proposed action : ', proposedAction);
-    executeAction(account, proposedAction, POOL_ID)
-    .then((res) => {
-      console.log('Action executed');
-      reqPoolStake(account, POOL_ID)
-      .then(newPoolStake => {
-        console.log('New stake in pool', newPoolStake.toString());
+    console.log('Checking if Warchest can perform the action');
+    Promise.all([
+      reqPoolGetAccountStakedBalance(account, ACCOUNT_ID, POOL_ID),
+      reqPoolGetAccountUnstakedBalance(account, ACCOUNT_ID, POOL_ID)
+    ]).then(([warchestAccountStakedBalance, warchestAccountUnstakedBalance]) => {
+      console.log('wc staked', c2h(warchestAccountStakedBalance), 'wc unstaked', c2h(warchestAccountUnstakedBalance))
+      const actionToExecute = generateActionToExecute(seatBalanceConfig, proposedAction, warchestAccountStakedBalance, warchestAccountUnstakedBalance);
+      if (!actionToExecute) {
+        console.warn(`Won't execute proposed action : ${proposedAction.method} with amount ${c2h(proposedAction.amount)}`) 
+        return;
+      }
+      console.log(`Executing action ${actionToExecute.method} with amount ${c2h(actionToExecute.amount)}`);
+      executeAction(account, actionToExecute, POOL_ID)
+      .then((res) => {
+        console.log('Action executed');
+        reqPoolGetTotalStakedBalance(account, POOL_ID)
+        .then(newPoolStake => {
+          console.log('New stake in pool', h2c(newPoolStake), 'New ratio', newPoolStake / seatPrice);
+        });
+      })
+      .catch((err) => {
+        console.error('Error while executing action', err);
       });
     })
-    .catch((err) => {
-      console.error('Error while executing action', err);
-    });
+
   })
 }
 
@@ -156,7 +219,7 @@ async function listen() {
   }, seatBalanceConfig.checkInterval * 1000)
 }
 const [nodeproc, procname, action] = process.argv
-console.log(process.argv)
+// console.log(process.argv)
 if (action && action == 'listen') {
   console.log('Listening')
   listen()
