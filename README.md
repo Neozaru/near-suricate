882c5b3a9400fd9acdfe5d6035d07643ea2f774f
Neozaru <neozaru@mailoo.org>
2020-06-29 10:51:02 +1200

    Proper CLI arguments parsing with `yargs`

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 DISCLAIMER : Proof of concept developed in the context of the NEAR stakewars competition (challenge004).
-Hard configured for betanet (for now).
-Published a l'arrache so I can submit the challenge and enjoy my Saturday =*
 
 ## Features
 In the Near network :
@@ -23,9 +21,9 @@ Run (will check/rebalance once)
 ```
 node dist/index.js
 ```
-Run (will check/rebalance every `watchInterval` -- see config below)
+Run (will check/rebalance every `[--interval]`)
 ```
-node dist/index.js --watch
+node dist/index.js watch
 ```
 
 ## Configuring
@@ -36,10 +34,11 @@ cp warchest-bot.config.json.example warchest-bot.config.json
 And modify it according to your needs :
 ```
 {
+  "networkId": "betanet",
+  "nodeUrl": "https://rpc.betanet.near.org",
   "nearKeystoreDir": "./neardev",
   "warchestAccountId": "neozaru14.betanet",
   "poolAccountId": "neozaru.stakehouse.betanet",
-  "watchInterval": 300,
   "rebalanceLevels": {
     "lowThreshold": 1.1,
     "lowTarget": 1.2,
@@ -50,11 +49,9 @@ And modify it according to your needs :
     "type": "BEST",
     "minRebalanceAmount": "1000000000000000000000000000"
   }
-
 }
 ```
 - `nearKeyStoreDir` should have the same structure as your `~/.near-credentials/` folder (you can actually set it to this folder). It should contain at least of keys associated with `warchestAccountId`.
-- `watchInterval` is the time to wait (in seconds) between checks/rebalancing.
 - `rebalanceLevels` defines at what levels (`lowThreshold`, `highThreshold`) the automatic rebalancing should be triggered and to what levels (`lowTarget`, `highTarget`) it should put the stake by rebalancing.
 Ex: If current seat price is 1000 and the currently staked balance in the pool is 1950, it will trigger the `highThreshold` (1950 > 1000 * `1.9`) and take action to set the staked balance in the pool to 1800 (`1.8` * 1000).
 - `rebalancePolicy` defines what to do when the warchest account does have enough fund deposited in the pool to meet the stake/unstake target. Setting the type to `BEST` will make the warchest account stake/unstake as many tokens as it can even if it is not enough to meet the `lowTarget`/`highTarget`. It is triggered only if the user can stake/unstake at least `minRebalanceAmount`.
--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,59 @@ import * as nearApi from 'near-api-js';
 import moment from 'moment';
 import fs from 'fs';
 
+import yargs from 'yargs';
 
-async function main(arg1) {
+function loadConfig(filename: string) {
+  const configData = fs.readFileSync(filename).toString();
+  return JSON.parse(configData);
+}
 
-  const configData = fs.readFileSync('./warchest-bot.config.json').toString();
-  const warchestBotConfig = JSON.parse(configData);
+async function buildWarchestBot(configFilename) {
+  const config = loadConfig(configFilename);
 
-  // TODO: Dynamic config
   const nearConnectionConfig = {
-    nodeUrl: 'https://rpc.betanet.near.org',
-    networkId: 'betanet',
+    nodeUrl: config.nodeUrl || 'https://rpc.betanet.near.org',
+    networkId: config.networkId || 'betanet',
     deps: {
-      keyStore: new nearApi.keyStores.UnencryptedFileSystemKeyStore(warchestBotConfig.nearKeystoreDir || undefined)
+      keyStore: new nearApi.keyStores.UnencryptedFileSystemKeyStore(config.nearKeystoreDir || undefined)
     }
   };
 
   const near = await nearApi.connect(nearConnectionConfig);
-  const warchestBot = new WarchestBot(near, warchestBotConfig);
+  return new WarchestBot(near, config);
+}
+
+async function main() {
+
+  const argv = yargs
+  .usage('Usage: $0 <command> [options]')
+  .example('$0 watch -i 3600', 'Checks and rebalances stake every hour')
+  .example('$0 -c custom-config.json', 'Checks and rebalances stake once with a custom config')
+  .command('watch', 'Keeps checking and rebalancing every <interval>', {
+    interval: {
+      alias: 'i',
+      type: 'number',
+      default: 300,
+    }
+  })
+  .alias('c', 'config')
+  .nargs('c', 1)
+  .describe('c', 'Path of config file')
+  .default('c', 'warchest-bot.config.json')
+  .help('h')
+  .alias('h', 'help')
+  .argv
+
+  const warchestBot = await buildWarchestBot(<string>argv.config);
   console.log(moment().format() + ' - Stake check');
   warchestBot.rebalance();
-  if (arg1 && arg1 === '--watch') {
+  if (argv._.includes('watch')) {
     setInterval(() => {
       console.log(moment().format() + ' - Stake check (watching)');
       warchestBot.rebalance();
-    }, warchestBotConfig.watchInterval * 1000)
+    }, <number>argv.interval * 1000)
 
   }
 }
-const [nodeproc, procname, arg1] = process.argv
 
-main(arg1);
+main();
